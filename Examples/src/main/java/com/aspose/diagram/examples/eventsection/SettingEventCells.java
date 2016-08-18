9d2e7c464d26e1a9437696f97266e288173029b3
Fahad Adeel <fahadadeel@gmail.com>
2016-08-18 17:05:29 +0500

    Aspose.Diagram for Java Examples

---
+++ Examples/src/main/java/com/aspose/diagram/examples/eventsection/SettingEventCells.java
@@ -0,0 +1,33 @@
+package com.aspose.diagram.examples.eventsection;
+
+import com.aspose.diagram.Diagram;
+import com.aspose.diagram.Page;
+import com.aspose.diagram.SaveFileFormat;
+import com.aspose.diagram.Shape;
+import com.aspose.diagram.examples.Utils;
+
+public class SettingEventCells {
+	public static void main(String[] args) throws Exception {
+		// The path to the documents directory.
+		String dataDir = Utils.getDataDir(SettingEventCells.class);
+		// load diagram
+		Diagram diagram = new Diagram(dataDir + "TestTemplate.vsdm");
+		// get page
+		Page page = diagram.getPages().get(0);
+		// get shape id
+		long shapeId = page.addShape(3.0, 3.0, 0.36, 0.36, "Square");
+		// get shape
+		Shape shape = page.getShapes().getShape(shapeId);
+
+		// set event cells in the ShapeSheet
+		shape.getEvent().getEventXFMod().getUfe().setF("CALLTHIS(\"ThisDocument.ShowAlert\")");
+		shape.getEvent().getEventXFMod().getUfe().setF("CALLTHIS(\"ThisDocument.ShowAlert\")");
+		shape.getEvent().getEventXFMod().getUfe().setF("CALLTHIS(\"ThisDocument.ShowAlert\")");
+		shape.getEvent().getEventXFMod().getUfe().setF("CALLTHIS(\"ThisDocument.ShowAlert\")");
+		shape.getEvent().getEventXFMod().getUfe().setF("CALLTHIS(\"ThisDocument.ShowAlert\")");
+		shape.getEvent().getEventXFMod().getUfe().setF("CALLTHIS(\"ThisDocument.ShowAlert\")");
+
+		// save diagram
+		diagram.save(dataDir + "Output_NET.vsdm", SaveFileFormat.VSDM);
+	}
+}
